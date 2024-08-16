e73103b4012bd2e143d9e92033e6b34be812ae27
8Mi_Yile <admin@8mi.tech>
2024-08-16 15:02:02 +0800

    Create BMIUploader189.java

---
+++ src/main/java/net/burningtnt/hmclfetcher/publish/uploaders/impl/BMIUploader189.java
@@ -0,0 +1,34 @@
+package net.burningtnt.hmclfetcher.publish.uploaders.impl;
+
+import net.burningtnt.hmclfetcher.publish.structure.ArchiveFile;
+import net.burningtnt.hmclfetcher.publish.structure.SourceBranch;
+import net.burningtnt.hmclfetcher.publish.uploaders.IUploadAction;
+import net.burningtnt.hmclfetcher.publish.uploaders.IUploader;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+public final class BMIUpload189 implements IUploader {
+    public BMIUpload189() {
+    }
+
+    @Override
+    public IUploadAction build(Path file, ArchiveFile info, SourceBranch source) {
+        return new IUploadAction() {
+            @Override
+            public void upload() {
+            }
+
+            @Override
+            public URI getResult() throws URISyntaxException {
+                return new URI("https://alist.8mi.tech/d/HMCL/189/" + info.getFileHash() + '/' + info.getFileName());
+            }
+        };
+    }
+
+    @Override
+    public String getUploaderID() {
+        return "8mi.189";
+    }
+}
